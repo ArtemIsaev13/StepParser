a3206e36812a5891d6c2680d377c82e19eb333d1
Isaev A <Don-Kison@rambler.ru>
2024-05-03 19:48:45 +0300

    Added cad engine analyzation

--- SimpleStepParser/SimpleStepParser.cs
+++ SimpleStepParser/SimpleStepParser.cs
@@ -2,32 +2,49 @@
 using SimpleStepParser.SimplifiedModelRepresentation._2.Application;
 using SimpleStepParser.StepFileRepresentation._1.Domain.StepRepresentation;
 using SimpleStepParser.StepFileRepresentation._2.Application.Parser;
-using System.Diagnostics;
 using System.Text;
 
 namespace SimpleStepParser
 {
     public static class SimpleStepParser
     {
-        public static Model? ReadStepFile(string path)
+        /// <summary>
+        /// Get parsed step file as a model tree with coordinate systems
+        /// </summary>
+        /// <param name="fullStepFile">Full step file as an array of strings</param>
+        /// <returns></returns>
+        public static Assembly? Parse(string[] fullStepFile)
         {
-            Stopwatch stopwatch = Stopwatch.StartNew();
+            StepRepresentation representation
+                = StepRepresentationParser.GetStepRepresentation(fullStepFile);
+
+            Model? resultRoot = ModelInterpretator.GetModelTree(representation);
+            CadName resultCadName = CadNameInterpretator.GetCadNameByHeader(representation.Header);
+
+            Assembly? result = new Assembly()
+            {
+                Root = resultRoot,
+                CadName = resultCadName
+            };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get parsed step file as a model tree with coordinate systems
+        /// </summary>
+        /// <param name="path">Path to step file</param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static Assembly? ReadStepFile(string path)
+        {
             if (!File.Exists(path))
             {
                 throw new FileNotFoundException();
             }
             var file = File.ReadAllLines(path, Encoding.UTF8);
-            Console.WriteLine($"File is readed {stopwatch.ElapsedMilliseconds}");
-            stopwatch.Restart();
 
-            StepRepresentation representation 
-                = StepRepresentationParser.GetStepRepresentation(file);
-            Console.WriteLine($"Step representation created {stopwatch.ElapsedMilliseconds}");
-            stopwatch.Restart();
-
-            Model? result = ModelInterpretator.GetModelTree(representation);
-            Console.WriteLine($"Model created {stopwatch.ElapsedMilliseconds}\n");
-            return result;
+            return Parse(file);
         }
     }
 }
