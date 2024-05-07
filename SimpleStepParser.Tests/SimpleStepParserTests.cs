88bf7d838b8ec0feed21d6073b15f5ca8b65a559
Isaev A <Don-Kison@rambler.ru>
2024-05-07 09:59:43 +0300

    Added a project for unit testing

---
+++ SimpleStepParser.Tests/SimpleStepParserTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace SimpleStepParser.Tests;
+
+public static class SimpleStepParserTests
+{
+
+
+    [Theory]
+    [InlineData(null)]
+    public static void SimpleStepParser_ReadStepFile_ShouldReturnProperModelTree(string path)
+    {
+
+    }
+}
